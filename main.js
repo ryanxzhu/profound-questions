f5cc7789867392c31c5569e6ea28aeb2ada7040a
Ryan Zhu <ryanxzhu@gmail.com>
2019-12-27 20:38:52 -0500

    added refresh icon to get the past 3 questions

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-console.log(questionsArray.length);
-document.body.style.height = window.innerHeight * 0.99 + 'px';
-document.body.style.fontSize = Math.sqrt(window.innerWidth * window.innerHeight) * 0.08 + 'px';
-document.body.style.padding = '0px ' + window.innerWidth * 0.05 + 'px';
+const refreshIcon = document.getElementById('refreshIcon');
 
-//document.body.textContent = questionsArray[Math.floor(Math.random() * questionsArray.length)];
-document.body.textContent = questionsArray[13];
+//---------------------Intiiate---------------------
+
+updateSizes();
+document.getElementsByTagName('div')[0].textContent = questionsArray[13];
+
+//---------------------Functions---------------------
 
 function updateSizes() {
-	document.body.style.width = window.innerWidth * 0.99 + 'px';
-	document.body.style.height = window.innerHeight * 0.99 + 'px';
-	document.body.style.padding = '0px ' + window.innerWidth * 0.05 + 'px';
-	document.body.style.fontSize = Math.sqrt(window.innerWidth * window.innerHeight) * 0.08 + 'px';
+	document.getElementsByTagName('div')[0].style.width = window.innerWidth * 0.99 + 'px';
+	document.getElementsByTagName('div')[0].style.height = window.innerHeight * 0.99 + 'px';
+	document.getElementsByTagName('div')[0].style.padding = '0px ' + window.innerWidth * 0.05 + 'px';
+	document.getElementsByTagName('div')[0].style.fontSize =
+		Math.sqrt(window.innerWidth * window.innerHeight) * 0.08 + 'px';
+	refreshIcon.style.width = window.innerWidth * 0.05 + 'px';
+	refreshIcon.style.margin = window.innerWidth * 0.01 + 'px';
 }
 
 function newQuestion() {
 	let randomInt = Math.floor(Math.random() * questionsArray.length);
-	document.body.textContent = questionsArray[randomInt];
+	document.getElementsByTagName('div')[0].textContent = questionsArray[randomInt];
 }
-document.body.addEventListener('mousedown', function() {
+
+//---------------------Events---------------------
+
+document.getElementsByTagName('div')[0].addEventListener('mousedown', function() {
 	newQuestion();
 });
 

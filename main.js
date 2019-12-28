bddfa3174a200e4874c47bbbf26f22960348cace
Ryan Zhu <ryanxzhu@gmail.com>
2019-12-27 21:03:27 -0500

    made refresh Icon easier to click by making the image background bigger and made the refreshIcon button

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const refreshIcon = document.getElementById('refreshIcon');
+var previousQuestion = [];
 
 //---------------------Intiiate---------------------
 
 updateSizes();
-document.getElementsByTagName('div')[0].textContent = questionsArray[13];
+newQuestion();
 
 //---------------------Functions---------------------
 
@@ -13,23 +14,29 @@ function updateSizes() {
 	document.getElementsByTagName('div')[0].style.padding = '0px ' + window.innerWidth * 0.05 + 'px';
 	document.getElementsByTagName('div')[0].style.fontSize =
 		Math.sqrt(window.innerWidth * window.innerHeight) * 0.08 + 'px';
-	refreshIcon.style.width = Math.sqrt(window.innerWidth * window.innerHeight) * 0.07 + 'px';
-	refreshIcon.style.margin =
-		Math.sqrt(window.innerWidth * window.innerHeight) * 0.02 +
-		'px ' +
-		Math.sqrt(window.innerWidth * window.innerHeight) * 0.05 +
-		'px';
+	refreshIcon.style.width = Math.sqrt(window.innerWidth * window.innerHeight) * 0.15 + 'px';
 }
 
 function newQuestion() {
+	if (previousQuestion.length === 2) {
+		previousQuestion.shift();
+	}
 	let randomInt = Math.floor(Math.random() * questionsArray.length);
 	document.getElementsByTagName('div')[0].textContent = questionsArray[randomInt];
+	previousQuestion.push(questionsArray[randomInt]);
 }
 
 //---------------------Events---------------------
 
 document.getElementsByTagName('div')[0].addEventListener('mousedown', function() {
 	newQuestion();
+});
+
+refreshIcon.addEventListener('mousedown', function() {
+	if (previousQuestion.length > 1) {
+		document.getElementsByTagName('div')[0].textContent = previousQuestion[0];
+		previousQuestion.pop();
+	}
 });
 
 window.addEventListener('resize', updateSizes);

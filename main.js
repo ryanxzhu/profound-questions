e9e78470d76961f00898df32999923ec1b909013
Ryan Zhu <ryanxzhu@gmail.com>
2019-12-27 20:09:43 -0500

    added noSelect class so that the text can't be selected. Also added font size responsiveness dependent on area of the screen

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 console.log(questionsArray.length);
 document.body.style.height = window.innerHeight * 0.99 + 'px';
-document.body.style.fontSize = window.innerWidth * 0.05 + 'px';
+document.body.style.fontSize = Math.sqrt(window.innerWidth * window.innerHeight) * 0.08 + 'px';
 document.body.style.padding = '0px ' + window.innerWidth * 0.05 + 'px';
 
 //document.body.textContent = questionsArray[Math.floor(Math.random() * questionsArray.length)];
-document.body.textContent = questionsArray[15];
+document.body.textContent = questionsArray[13];
+
+function updateSizes() {
+	document.body.style.width = window.innerWidth * 0.99 + 'px';
+	document.body.style.height = window.innerHeight * 0.99 + 'px';
+	document.body.style.padding = '0px ' + window.innerWidth * 0.05 + 'px';
+	document.body.style.fontSize = Math.sqrt(window.innerWidth * window.innerHeight) * 0.08 + 'px';
+}
 
 function newQuestion() {
 	let randomInt = Math.floor(Math.random() * questionsArray.length);
@@ -14,13 +21,5 @@ document.body.addEventListener('mousedown', function() {
 	newQuestion();
 });
 
-var questionsLength = [];
-for (let i = 0; i < questionsArray.length; i++) {
-	questionsLength.push(questionsArray[i].length);
-}
-
-questionsLength.sort(function(a, b) {
-	return a - b;
-});
-
-console.log(questionsLength);
+window.addEventListener('resize', updateSizes);
+window.addEventListener('orientationchange', updateSizes);

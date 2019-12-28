4329f8ef437c83dbbe6b68e98f4023df17957add
Ryan Zhu <ryanxzhu@gmail.com>
2019-12-27 19:52:51 -0500

    trying to make the font size responsive

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 console.log(questionsArray.length);
 document.body.style.height = window.innerHeight * 0.99 + 'px';
-document.body.style.fontSize = window.innerHeight * 0.1 + 'px';
+document.body.style.fontSize = window.innerWidth * 0.05 + 'px';
 document.body.style.padding = '0px ' + window.innerWidth * 0.05 + 'px';
 
-document.body.textContent = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+//document.body.textContent = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+document.body.textContent = questionsArray[15];
 
+function newQuestion() {
+	let randomInt = Math.floor(Math.random() * questionsArray.length);
+	document.body.textContent = questionsArray[randomInt];
+}
 document.body.addEventListener('mousedown', function() {
-	document.body.textContent = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+	newQuestion();
 });
+
+var questionsLength = [];
+for (let i = 0; i < questionsArray.length; i++) {
+	questionsLength.push(questionsArray[i].length);
+}
+
+questionsLength.sort(function(a, b) {
+	return a - b;
+});
+
+console.log(questionsLength);
